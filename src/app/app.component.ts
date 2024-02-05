b6a3ac21b070f313817774957f9e19bc5c752a72
cdan-221 <chiaremari@gmail.com>
2024-02-04 21:46:43 -0500

    initializing skid cards with skid array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { SkidComponent } from './skid/skid.component';
+import { Skid } from './skid/skid.component';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,25 @@ import { SkidComponent } from './skid/skid.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'digital-forms-bootstrap';
-  skids = Array(10).fill(0);  // Creates an array with 10 elements
+  numberOfSkids = 2; // 16; 
+  skids: Skid[] = Array.from({length: this.numberOfSkids}, (_, i) => (
+    { number: i + 1, 
+      type: '',
+      packaging: ''
+    }
+  ))
+
+  private _skids = new BehaviorSubject<Skid[]>(this.skids);
+  skids$ = this._skids.asObservable();
+
+  updateSkids(updatedSkid: Skid, index: number) {
+    let skids = this._skids.getValue();
+    skids[index] = updatedSkid;
+    this._skids.next(skids);
+    console.log('skids:', skids);
+  }
+
 }
--- src/app/skid/skid.component.ts
+++ src/app/skid/skid.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+
+export interface Skid {
+  number: number;
+  type: string;
+  packaging: string;
+}
 
 @Component({
   selector: 'skid',
@@ -7,6 +14,41 @@ import { Component } from '@angular/core';
   templateUrl: './skid.component.html',
   styleUrl: './skid.component.css'
 })
-export class SkidComponent {
 
+export class SkidComponent {
+  @Input() skid!:  Skid;
+  @Output() skidChanged = new EventEmitter<Skid>();
+  private _skid = new BehaviorSubject<Skid>(this.skid);
+  skid$ = this._skid.asObservable();
+
+  constructor() {
+    this.skid$.subscribe(skid => {
+      this.updateSkid(skid)
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['skid']) {
+      this._skid.next(this.skid);
+    }
+  }
+  
+  updateSkid(updatedSkid: Skid) {
+    let skid = this._skid.getValue();
+    if (JSON.stringify(skid) !== JSON.stringify(updatedSkid)) {
+      this._skid.next(updatedSkid);
+    }
+    this.skidChanged.emit(this.skid);
+    //not sure why this is printing more than expected
+    //console.log('skid:', skid);
+
+  }
+
+  openTypeDialog(): void {
+    // open a dialog to select the type
+
+    console.log('openTypeDialog');
+
+  }
+  
 }
